4d08b775283621155b63be7ec9785a3d0cdc50aa
shubha-mm <167985010+shubha-mm@users.noreply.github.com>
2024-10-07 11:44:56 +0530

    loginpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,15 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'logout',
+    loadChildren: () => import('./pages/logout/logout.module').then( m => m.LogoutPageModule)
+  },
 ];
 
 @NgModule({
