5b286f5c41a6489763575ea8e20f9b9e07458843
Saksham Shakya <Sakshamshakya319@gmail.com>
2024-10-17 21:51:31 +0530

    home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,8 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  courses = ['Course 1', 'Course 2', 'Course 3','Course 3'];
-  books = ['Book 1', 'Book 2', 'Book 3','Book 4'];
+  courses = [
+    { title: 'Course 1', image: 'assets/img/cyber 5.jpg' },
+    { title: 'Course 2', image: 'assets/img/Ai 2 image.jpg' },
+    { title: 'Course 3', image: 'assets/img/DATA sci 2.jpg' },
+    { title: 'Course 4', image: 'assets/img/data sci 5.jpg' },
+  ];
+  books = [
+    { title: 'Book 1', image: 'assets/img/1.jpg' },
+    { title: 'Book 2', image: 'assets/img/3.png' },
+    { title: 'Book 3', image: 'assets/img/4.png' },
+    { title: 'Book 4', image: 'assets/img/cyber 5.jpg' },
+  ];
   tests = ['Test 1', 'Test 2', 'Test 3', 'Test 4'];
 
   constructor(private authService: AuthService) {}
