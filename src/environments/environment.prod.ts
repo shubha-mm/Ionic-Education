b6e26a786d3ae2450d9ba71555892befcaab2692
Saksham Shakya <Sakshamshakya319@gmail.com>
2024-10-21 10:52:53 +0530

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB1ZA_NbuYmAPNT4BLuo9KW9CKVucc30Fg",
+  authDomain: "login-test-3ba5f.firebaseapp.com",
+  projectId: "login-test-3ba5f",
+  storageBucket: "login-test-3ba5f.appspot.com",
+  messagingSenderId: "593503431658",
+  appId: "1:593503431658:web:ee7351c04626eab5882093"
+ }
 };
